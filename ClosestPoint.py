3b1b1c26dbd37b9348e99d841581209b2cd1ed54
DTORERO <dtorero@minedu.gob.pe>
2018-01-31 17:29:07 -0500

    Created a Class ArcPoint to manage Attribute tables data. Arcpoint Class and distance method needs to be completed and tested.

--- ClosestPoint.py
+++ ClosestPoint.py
@@ -5,8 +5,81 @@ import numpy as np
 import math
 import arcpy
 
+class ArcPoint(object):
+    """
+    Container for point information extracted from an ArcGis shapefile
+    """
 
-def SHPtoLatLong(shapefile, spatialReferenceCode=4326 ):
+    def __init__(self, id, aTable, coords):
+        """
+        :param id: ObjectID in the shapefile
+        :param aTable: attribute table from the shapefile
+                becomes a dictionary containing fields and values (apart from to ObjectID or shape)
+        :param coords: nympy array containing latitude and longitude
+        """
+        if isinstance(id, int):
+            self.id = id
+        else:
+            raise ValueError("id value should be an int - ObjectID from the shapefile")
+
+        if isinstance(aTable, dict):
+            self.aTable = aTable
+        else:
+            raise ValueError("Attribute table should be transformed into a dictionary datatype")
+
+        if isinstance(coords, numpy.array):
+            self.coords = coords
+        else:
+            raise ValueError("Coordinates should be transformed into a numpy array")
+
+    def get_id(self):
+        return self.id
+
+    def get_attributes(self):
+        return self.aTable[:]
+
+    def get_fields(self):
+        return self.aTable.keys()
+
+    def get_field_value(self, field):
+        return self.aTable[field]
+
+    def get_coords(self):
+        return self.coords.copy()
+
+    def distance(self, other):
+        """
+        Calculates the distance between two ArcPoint objects,
+        from its coordinates values using the Haversine formula
+        :param other: ArcPoint Object to calculate the distance to
+        :return:
+        """
+
+        """
+        Calculates the distance between two ArcPoint objects,
+        from its coordinates values using the Haversine formula
+        
+        
+        :param PointO: array([Latitude O,Longitude O])
+        :param PointU: array([Latitude U,Longitude U])
+        :return: distance between two points
+        """
+        R = 6371  # Radius of the earth in km
+        D = PointU - PointO
+        dLat = hexToRad(D[0])
+        dLon = hexToRad(D[1])
+
+        # Haversine formula for calculating the distance
+        a = math.sin(dLat / 2) ** 2 + math.cos(hexToRad(PointO[0])) * math.cos(hexToRad(PointU[0])) * math.sin(
+            dLon / 2) * math.sin(dLon / 2)
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+        distance = R * c
+
+        return distance
+
+
+
+def SHPtoLatLong(shapefile, spatialReferenceCode = 4326 ):
     """
     :param shapefile: shapefile path with Point Features only
     :param spatialReferenceCode: Name of the Spatial reference. Default is WGS84
